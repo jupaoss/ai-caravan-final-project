0f6ef0ac3dd7e3f701d652c39a2d988bc6678d49
Julián Patiño <julianpatinoossa@gmail.com>
2026-04-29 19:48:11 -0500

    Animated Routes Clen Up

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -4,7 +4,6 @@ import { AnimatePresence } from 'framer-motion'
 import { EntryPoint } from '../pages/EntryPoint'
 import { EchoOnboarding } from '../pages/EchoOnboarding'
 import { GazeShop } from '../pages/GazeShop'
-import { EchoShop } from '../pages/EchoShop'
 import { EchoShopV2 } from '../pages/EchoShopV2'
 import { ProductDetail } from '../pages/ProductDetail'
 
@@ -16,7 +15,6 @@ export const AnimatedRoutes = () => {
         <Route path="/" element={<EntryPoint />} />
         <Route path="/gaze" element={<GazeShop />} />
         <Route path="/echo-onboarding" element={<EchoOnboarding />} />
-        <Route path="/echo" element={<EchoShop />} />
         <Route path="/echo-v2" element={<EchoShopV2 />} />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
